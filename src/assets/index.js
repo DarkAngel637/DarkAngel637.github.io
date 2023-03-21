8f995337d17d34248ae2af19b646dd3bb5de0e9b
guojunhao <269099484@qq.com>
2023-03-21 22:51:02 +0800

    初始化提交

---
+++ src/assets/index.js
@@ -0,0 +1,11 @@
+/*
+ * @Author: Dark Angel
+ * @Date: 2023-03-20 23:31:31
+ * @LastEditTime: 2023-03-20 23:41:17
+ * @LastEditors: Dark Angel
+ * @Description: 干就完事了!
+ * @FilePath: \DarkAngel637.github.io\src\assets\index.js
+ */
+import imgs from "./imgs/";
+
+export default imgs;
